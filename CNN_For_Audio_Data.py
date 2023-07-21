5b134d348e8b69756320a6735b5ff244931091a1
Arkadeep Das <arkadeep.das8@gmail.com>
2023-07-21 12:12:23 +0530

    CNN architecture

--- CNN_For_Audio_Data.py
+++ CNN_For_Audio_Data.py
@@ -1,6 +1,8 @@
 # Previously we see that we have audio with multiple channels
 # Now after preprocessing the data and convert it to 3D of the data we apply CNN on it
-# Dimentions: (Channels x Audio Data) -> (Channels x Features x Mel frequency bins)
+# Dimentions: (Channels x Audio Data) -> (Channels x Features/frequency axis x Mel frequency bins)
+
+# Here we assume that it's a classification problem. The model will predict 10 classes.
 
 import torch
 from torch import nn
@@ -49,5 +51,33 @@ class CNNNetwork(nn.Module):
             nn.MaxPool2d(kernel_size=2),
         )
         self.flatten = nn.Flatten()
-        self.linear = nn.Linear(in_features=128 * 5 * 4, out_features=10)
+        # Check the shape from the previous layer and set the input features in linear layer
+        self.linear = nn.Linear(in_features=128 * 4 * 2, out_features=10)
         self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        # Applying 4 Convolution layers
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.conv3(x)
+        x = self.conv4(x)
+        # Applying Flatten layer
+        x = self.flatten(x)
+        # Applying Linear layer
+        x = self.linear(x)
+        # Applying softmax
+        output = self.softmax(x)
+        return output
+
+
+# # Let's test the model
+# def test():
+#     # Input image size is 256x256
+#     x = torch.randn((1, 1, 64, 44))
+#     model = CNNNetwork()
+#     pred = model(x)
+#     # We get the shape 30x30
+#     print(pred.shape)
+
+# if __name__ == '__main__':
+#     test()
