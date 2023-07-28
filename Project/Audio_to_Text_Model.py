d2400a3affc9eb8f291144633428bccb2f9c2a71
Arkadeep Das <arkadeep.das8@gmail.com>
2023-07-28 12:14:17 +0530

    LSTM and Linear Block

--- Project/Audio_to_Text_Model.py
+++ Project/Audio_to_Text_Model.py
@@ -11,15 +11,15 @@ import torch.nn as nn
 # Let's create the convolution block
 class CNNBlock(nn.Module):
 
-    def __init__(self, in_channels=1):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
         self.cnn_1 = nn.Conv2d(in_channels=in_channels,
-                               out_channels=32,
+                               out_channels=out_channels,
                                kernel_size=3,
                                padding=1)
         self.batch_norm_1 = nn.BatchNorm2d(num_features=32)
         self.cnn_2 = nn.Conv2d(in_channels=32,
-                               out_channels=64,
+                               out_channels=out_channels * 2,
                                kernel_size=3,
                                padding=1)
         self.batch_norm_2 = nn.BatchNorm2d(num_features=64)
@@ -38,8 +38,49 @@ class CNNBlock(nn.Module):
 
 
 # Let's create LSTM and Linear block
-# class LSTMBlock(nn.Module):
+class LSTMBlock(nn.Module):
+
+    def __init__(self, number_of_classes=115):
+        super().__init__()
+        self.lstm = nn.LSTM(input_size=0, hidden_size=128, num_layers=4)
+        self.linear_1 = nn.Linear(in_features=128, out_features=256)
+        self.linear_2 = nn.Linear(in_features=256, out_features=512)
+        self.linear_3 = nn.Linear(in_features=512, out_features=256)
+        self.linear_4 = nn.Linear(in_features=256,
+                                  out_features=number_of_classes)
+        self.batch_norm_1 = nn.BatchNorm1d(256)
+        self.batch_norm_2 = nn.BatchNorm1d(512)
+        self.batch_norm_3 = nn.BatchNorm1d(256)
+        self.relu = nn.ReLU()
+        self.flatten = nn.Flatten()
+
+    def forward(self, x):
+        x = self.lstm(x)
+        x = self.flatten(x)
+        x = self.linear_1(x)
+        x = self.batch_norm_1(x)
+        x = self.relu(x)
+        x = self.linear_2(x)
+        x = self.batch_norm_2(x)
+        x = self.relu(x)
+        x = self.linear_3(x)
+        x = self.batch_norm_3(x)
+        x = self.relu(x)
+        x = self.linear_4(x)
+        return x
+
+
+# class Audio_To_Text_Model(nn.Module):
 
 #     def __init__(self):
-#         super().__init__(number_0f_classes=115)
-#         self.lstm = nn.LSTM(input_size = , hidden_size)
+#         super().__init__()
+#         self.cnnblock_1 = CNNBlock(in_channels=1, out_channels=32)
+#         self.cnnblock_2 = CNNBlock(in_channels=64, out_channels=)
+
+
+# data = torch.randn((1, 1, 64, 4073))
+# model = CNNBlock(in_channels=1)
+# output = model(data)
+# output = model(output)
+# output = model(output)
+# print(output.shape)
